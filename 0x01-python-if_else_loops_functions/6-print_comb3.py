86608402343d69ad80fc7c51f2bd12fb09aca8c8
John Gaitho <johngaitho05@gmail.com>
2023-08-08 17:27:43 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(10):
         if (j * 10 + i) < (i * 10 + j) or i == j:
             continue
         if (i * 10 + j) != 89:
-            print(i * 10 + j, end=', ')
+            print(('0' if (i * 10 + j) < 10 else '') + str(i * 10 + j), end=', ')
 print(89)
