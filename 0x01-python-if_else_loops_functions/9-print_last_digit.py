37b1b7298453e852234895350182e0a7014d3fbe
John Gaitho <johngaitho05@gmail.com>
2023-08-08 18:16:19 +0300

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    if type(number) != int:
+        raise ValueError("Not a valid number")
     res = str(number)[-1]
     print(res, end='')
     return res
