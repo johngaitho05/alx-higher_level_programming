88c296fb864625cf15b66f98d1e4ca35959a9258
John Gaitho <johngaitho05@gmail.com>
2023-08-16 15:57:52 +0300

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,17 +3,6 @@ def roman_to_int(roman_string):
     _map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not roman_string or any(c not in _map for c in roman_string):
         return 0
-    count = 1
-    letter = roman_string[0]
-    for i in range(1, len(roman_string)):
-        new_letter = roman_string[i]
-        if _map[new_letter] > _map[letter] and count > 1:
-            return 0
-        if new_letter != letter:
-            count = 1
-        else:
-            count += 1
-        letter = new_letter
     res, i, length = 0, 0, len(roman_string) - 1
     while i <= length:
         current = _map[roman_string[i]]
@@ -25,4 +14,3 @@ def roman_to_int(roman_string):
         i += 1
 
     return res
-
