47f0a264f9636560eba91bf0ec9cc14e911ac134
John Gaitho <johngaitho05@gmail.com>
2023-08-16 17:50:49 +0300

    cpython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -7,12 +7,9 @@
  */
 void print_python_list(PyObject *p)
 {
-	PyObject *item;
-	Py_ssize_t size;
-
 	if (PyList_Check(p))
 	{
-		size = PyList_Size(p);
+		Py_ssize_t size = PyList_Size(p);
 
 		printf("[*] Python list info\n");
 		printf("[*] Size of the Python List = %ld\n", size);
@@ -20,7 +17,7 @@ void print_python_list(PyObject *p)
 		printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 		for (Py_ssize_t i = 0; i < size; i++)
 		{
-			item = PyList_GetItem(p, i);
+			PyObject *item = PyList_GetItem(p, i);
 
 			printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 		}
@@ -37,13 +34,10 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size;
-	char *bytes;
-
 	if (PyBytes_Check(p))
 	{
-		size = PyBytes_Size(p);
-		bytes = PyBytes_AsString(p);
+		Py_ssize_t size = PyBytes_Size(p);
+		char *bytes = PyBytes_AsString(p);
 
 		printf("[.] bytes object info\n");
 		printf("  [.] size: %ld\n", size);
@@ -57,9 +51,8 @@ void print_python_bytes(PyObject *p)
 				printf(" ");
 		}
 		printf("\n");
-	}
-	else
+	} else
 	{
-		fprintf(stderr, "  [ERROR] Invalid Bytes Object\n");
+		fprintf(stderr, "Invalid Bytes Object\n");
 	}
 }
