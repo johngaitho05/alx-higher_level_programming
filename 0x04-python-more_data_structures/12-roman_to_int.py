d96e26137127945c69a8d1eaf706746446c4fbba
John Gaitho <johngaitho05@gmail.com>
2023-08-16 15:05:45 +0300

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     _map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string or any(c not in _map for c in roman_string):
+    if not roman_string:
         return 0
-
+    if any(c not in _map for c in roman_string):
+        raise ValueError("Invalid roman number")
     res, i, length = 0, 0, len(roman_string) - 1
 
     while i <= length:
