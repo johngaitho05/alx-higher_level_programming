2470f5b1e63b0c187da8d916b63a7a046f1eb4f9
John Gaitho <johngaitho05@gmail.com>
2023-08-16 13:23:44 +0300

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if not (my_list and search and replace):
-        return
+        return my_list
     return [replace if v == search else v for v in my_list]
