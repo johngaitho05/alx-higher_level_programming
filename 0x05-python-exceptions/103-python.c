4d7fb81eb52c4e0d8ee2625bb08f187e9bf5de4b
John Gaitho <johngaitho05@gmail.com>
2023-08-28 23:32:15 +0300

    updates

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,9 +1,10 @@
 #include <Python.h>
 #include <stdio.h>
 
-#define bytes_to_str(op) (((PyBytesObject *)(op))->ob_sval)
-#define float_as_double(op) (((PyFloatObject *)(op))->ob_fval)
-#define custom_get_size(op) (((PyVarObject *)(op))->ob_size)
+#define custom_pybtes_as_string(op) (((PyBytesObject *)(op))->ob_sval)
+#define custom_pyfloat_as_double(op) (((PyFloatObject *)(op))->ob_fval)
+#define custom_pybytes_get_size(op) (((PyVarObject *)(op))->ob_size)
+#define custom_pylist_size(op) (((PyVarObject *)(op))->ob_size)
 
 static inline PyTypeObject *custom_py_type(PyObject *ob)
 {
@@ -42,54 +43,29 @@ PyObject *retrieve_item(PyObject *op, Py_ssize_t i)
 }
 
 /**
- * print_python_list - Prints information about a Python list object
- * @p: A pointer to the PyObject representing the list
- */
-void print_python_list(PyObject *p)
-{
-	if (!PyList_Check(p))
-	{
-		fprintf(stderr, "[ERROR] Invalid PyListObject\n");
-		return;
-	}
-
-	Py_ssize_t size = PyList_Size(p);
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", ((PyListObject *) p)->allocated);
-	for (Py_ssize_t i = 0; i < size; ++i)
-	{
-		PyObject *item = retrieve_item(p, i);
-
-		printf("Element %zd: %s\n", i, custom_py_type(item)->tp_name);
-	}
-}
-
-/**
  * print_python_bytes - Prints information about a Python bytes object
  * @p: A pointer to the PyObject representing the bytes object
  */
 void print_python_bytes(PyObject *p)
 {
+	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		fprintf(stderr, "[ERROR] Invalid PyBytesObject\n");
+		fprintf(stderr, "  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
-	printf("[.] bytes object info\n");
-	printf("  size: %zd\n", custom_get_size(p));
-	printf("  trying string: %s\n", bytes_to_str(p));
-	printf("  first %zd bytes: ", custom_get_size(p) + 1);
-	if (custom_get_size(p) >= 10)
+	setbuf(stdout, NULL);
+	printf("  size: %zd\n", custom_pybytes_get_size(p));
+	printf("  trying string: %s\n", custom_pybtes_as_string(p));
+	printf("  first %zd bytes: ", custom_pybytes_get_size(p) + 1);
+	if (custom_pybytes_get_size(p) >= 10)
 	{
 		for (int i = 0; i < 10; ++i)
-			printf("%02hhx ", *((char *) (bytes_to_str(p)) + i));
+			printf("%02hhx ", *((char *) (custom_pybtes_as_string(p)) + i));
 	} else
 	{
-		for (Py_ssize_t i = 0; i <= custom_get_size(p); ++i)
-			printf("%02hhx ", *((char *) (bytes_to_str(p)) + i));
+		for (Py_ssize_t i = 0; i <= custom_pybytes_get_size(p); ++i)
+			printf("%02hhx ", *((char *) (custom_pybtes_as_string(p)) + i));
 	}
 	printf("\n");
 }
@@ -100,14 +76,46 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_float(PyObject *p)
 {
+	setbuf(stdout, NULL);
+	printf("[.] float object info\n");
 	if (!PyFloat_Check(p))
 	{
-		fprintf(stderr, "[ERROR] Invalid PyFloatObject\n");
+		fprintf(stderr, "  [ERROR] Invalid Float Object\n");
 		return;
 	}
 
-	double value = float_as_double(p);
+	double value = custom_pyfloat_as_double(p);
 
-	printf("[.] float object info\n");
-	printf("  value: %f\n", value);
+	printf("  value: %.15g\n", value);
+}
+
+
+/**
+ * print_python_list - Prints information about a Python list object
+ * @p: A pointer to the PyObject representing the list
+ */
+void print_python_list(PyObject *p)
+{
+	printf("[*] Python list info\n");
+	if (!PyList_Check(p))
+	{
+		fprintf(stderr, "  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	Py_ssize_t size = custom_pylist_size(p);
+
+	setbuf(stdout, NULL);
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", ((PyListObject *) p)->allocated);
+	for (Py_ssize_t i = 0; i < size; ++i)
+	{
+		PyObject *item = retrieve_item(p, i);
+
+		printf("Element %zd: %s\n", i, custom_py_type(item)->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
+		else if (PyFloat_Check(item))
+			print_python_float(item);
+	}
 }
