1393400ea03a169e6bc588657d27d58cb8323863
John Gaitho <johngaitho05@gmail.com>
2023-09-05 09:57:22 +0300

    0-add-integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-"""Program for adding two numbers"""
+"""
+Program for adding 2 numbers
+a and b must be integers or floats, otherwise raise a TypeError
+a and b must be first cast to integers if they are float
+"""
 
 
 def add_integer(a, b=98):
@@ -8,9 +12,11 @@ def add_integer(a, b=98):
     Add two integers. If either is float, cast it to integer
     :arg a: an integer or float
     :arg b: an integer or float
+    :return: a + b
     """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
+
