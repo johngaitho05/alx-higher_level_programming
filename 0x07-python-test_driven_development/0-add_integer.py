ab36d7d03c978f677902ae09a5f41619d88c06bb
John Gaitho <johngaitho05@gmail.com>
2023-09-06 11:07:04 +0300

    handle infinity

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,8 +17,8 @@ def add_integer(a, b=98):
     Raises:
         TypeError: If `a` or `b` is not of type int or float.
     """
-    if type(a) not in (int, float):
+    if type(a) not in (int, float) or a == float('inf'):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if type(b) not in (int, float) or b == float('inf'):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
