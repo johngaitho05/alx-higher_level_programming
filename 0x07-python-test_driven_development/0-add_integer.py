19876a7272ef948908e31ee4e468e9fd75e1ee03
John Gaitho <johngaitho05@gmail.com>
2023-09-06 10:59:17 +0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,16 +6,19 @@ Program for adding 2 numbers
 
 
 def add_integer(a, b=98):
+    """Adds two integers
 
-    """
-    Function that adds 2 integers. a and b must be integers or floats,
-    otherwise TypeError exception is raised with the message
-    a must be an integer or b must be an integer. a and b are also
-    cast into integer if they are float.
-    """
+    Args:
+        a (int | float): First integer.
+        b (:obj:`int|float`, optional): Second integer. Defaults to 98.
 
-    if not (isinstance(a, int) or isinstance(a, float)):
+    Returns:
+        The sum of the two integers.
+    Raises:
+        TypeError: If `a` or `b` is not of type int or float.
+    """
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int) or isinstance(b, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
