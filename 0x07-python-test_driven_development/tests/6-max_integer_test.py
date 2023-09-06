d78809b0487d9e29477cc61b9571ebc5a348aa04
John Gaitho <johngaitho05@gmail.com>
2023-09-06 11:36:47 +0300

    update tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,6 +2,7 @@
 """Unittest for max_integer([..])
 """
 import unittest
+
 max_integer = __import__('6-max_integer').max_integer
 
 
@@ -9,38 +10,32 @@ class TestMaxInteger(unittest.TestCase):
     """
     Defines test cases for max_integer function
     """
+
     def test_list_has_items(self):
         """
         test_list_has_items is a method. Calls max_integer with
         an empty list and verifies if None is returned
         """
-        max_value = max_integer([])
-        self.assertIsNone(max_value)
+        self.assertIsNone(max_integer([]))
 
     def test_returns_max(self):
         """
-        test_return_max checks whether the the max integer is
+        test_return_max checks whether the max integer is
         returned given valid inputs
         """
-        max_value = max_integer([2, 7, 9])
-        expected = 9
-        self.assertEqual(max_value, expected)
+        self.assertEqual(max_integer([2, 7, 9]), 9)
 
     def test_negative_values(self):
         """
         test the function given negative integers as input
         """
-        max_value = max_integer([-1, 0, -5])
-        expected = 0
-        self.assertEqual(max_value, expected)
+        self.assertEqual(max_integer([-1, 0, -5]), 0)
 
     def test_same_values(self):
         """
         test the function given similar values
         """
-        max_value = max_integer([1, 1])
-        expected = 1
-        self.assertEqual(max_value, expected)
+        self.assertEqual(max_integer([1, 1]), 1)
 
     def test_string_values(self):
         """
@@ -52,12 +47,17 @@ class TestMaxInteger(unittest.TestCase):
         """
         test the function given floating point values
         """
-        max_value = max_integer([1.7, 2.2])
-        expected = 2.2
-        self.assertEqual(max_value, expected)
+        self.assertEqual(max_integer([1.7, 2.2]), 2.2)
 
     def test_list(self):
         """
         test the function given a list as input
         """
-        self.assertRaises(TypeError, max_integer, [1, [2, 3, 6]])
+        self.assertRaises(TypeError, max_integer, [1, [2, 3, 4]])
+        self.assertRaises(TypeError, max_integer, ['Hi', 1, 2])
+
+    def test_infinity(self):
+        """
+        test the function call with infinity
+        """
+        self.assertEqual(max_integer([100, -100, float('inf')]), float('inf'))
