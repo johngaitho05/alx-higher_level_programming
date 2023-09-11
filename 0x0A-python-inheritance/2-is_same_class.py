4546d6e1be4b8b74c94892ac282b5d3d536c5b98
John Gaitho <johngaitho05@gmail.com>
2023-09-11 21:46:39 +0300

    issubclass

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""Checks whether an object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Check if object is instance of class"""
+    return issubclass(a_class, obj.__class__)
