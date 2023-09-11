fa025ea392c1f5004056a52644439642a1d49ab2
John Gaitho <johngaitho05@gmail.com>
2023-09-11 23:54:39 +0300

    add attribute

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,14 +2,9 @@
 """Add custom attribute to an object"""
 
 
-def add_attribute(obj, name, value):
+def add_attribute(obj, att, value):
     """Add attribute to an object"""
-    immutable_types = [int, str, float, tuple,
-                       frozenset, bytes, bytearray, bool]
-
-    if obj is None:
+    if hasattr(obj, "__dict__"):
+        setattr(obj, att, value)
+    else:
         raise TypeError("can't add new attribute")
-    for _type in immutable_types:
-        if isinstance(obj, _type):
-            raise TypeError("can't add new attribute")
-    setattr(obj, name, value)
