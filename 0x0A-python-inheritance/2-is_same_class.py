31524bb410a446d2f313abd64f8ae831c830d111
John Gaitho <johngaitho05@gmail.com>
2023-09-11 21:51:28 +0300

    isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Checks whether an object is an instance of a class"""
+""" returns True if the object is exactly an instance
+of the specified class ; otherwise False"""
 
 
 def is_same_class(obj, a_class):
-    """Check if object is instance of class"""
+    """issubclass"""
     return issubclass(a_class, obj.__class__)
