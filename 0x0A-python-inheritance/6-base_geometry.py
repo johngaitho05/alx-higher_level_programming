ce97e09958747de0c6db4ff2fc4511644627b578
John Gaitho <johngaitho05@gmail.com>
2023-09-11 22:12:38 +0300

    not implemented

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Get ready for some calculations"""
+
+
+class BaseGeometry:
+    """I love geometry"""
+    pass
+
+    def area(self):
+        """Too lazy to do this"""
+        raise NotImplementedError("area() is not implemented")
