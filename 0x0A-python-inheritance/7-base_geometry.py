169bab38b1599129dfb1c2936384dcfebf5acf78
John Gaitho <johngaitho05@gmail.com>
2023-09-11 22:22:14 +0300

    base geaometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Get ready for some calculations"""
+
+
+class BaseGeometry:
+    """I love geometry"""
+
+    def area(self):
+        """Too lazy to do this"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Since I'm lazy, I should at least make sure others are not"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
