3b74505ea93c799caf9065d038afcec162ecc8ed
John Gaitho <johngaitho05@gmail.com>
2023-09-12 22:08:17 +0300

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -14,7 +14,7 @@ You are not allowed to import any module
 
 def class_to_json(obj):
     """
-    Function that returns the dictionary description with simple
+    Returns the dictionary description with simple
     data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
