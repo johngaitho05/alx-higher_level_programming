3588a075814832331a6acb0908a09b1b43148cf8
John Gaitho <johngaitho05@gmail.com>
2023-09-12 22:23:52 +0300

    student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -21,12 +21,13 @@ class Student:
     """Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Initialize"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """Returns the specified student attributes"""
-        if not attrs or type(attrs) != list:
+        if type(attrs) != list:
             return self.__dict__
         return {k: v for k, v in self.__dict__.items() if k in attrs}
