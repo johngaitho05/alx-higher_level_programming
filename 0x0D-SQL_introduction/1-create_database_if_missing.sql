b4c313bcf0180b7ee82ddf0063e88d9b8df6f868
John Gaitho <johngaitho05@gmail.com>
2023-10-17 20:40:29 +0300

    drop db

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- This script creates a database if it does not exist
+-- This script creates a database named hbtn_0c_0  if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- This script deletes a database named hbtn_0c_0 if it exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
