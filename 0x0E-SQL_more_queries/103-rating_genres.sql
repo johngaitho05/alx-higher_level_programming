1056aee17b7a8ccea303f0375168685aaf8e5df8
John Gaitho <johngaitho05@gmail.com>
2023-10-18 23:39:43 +0300

    query 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,6 @@
 -- This script lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tg.name as name, SUM(tsr.rate) AS rating
+SELECT tg.name AS name,
+SUM(tsr.rate) AS rating
 FROM tv_show_ratings tsr
 RIGHT JOIN tv_show_genres tsg
 ON tsg.show_id=tsr.show_id
