9e05d006a971542caa98eb9f4f41630180aeec84
John Gaitho <johngaitho05@gmail.com>
2023-11-14 23:49:54 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -3,5 +3,5 @@ const { list } = require('./100-data');
 
 const newList = list.map((value, index) => value * index);
 
-console.log('Original List:', list);
-console.log('New List:', newList);
+console.log(list);
+console.log(newList);
