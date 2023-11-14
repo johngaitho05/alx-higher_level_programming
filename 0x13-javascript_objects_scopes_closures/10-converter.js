b7a9ed984cb15cdb85cef10c35af792c11c93fab
John Gaitho <johngaitho05@gmail.com>
2023-11-14 22:29:02 +0300

    convert to base

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  return function convert (number) {
-    return number === 0 ? '0' : convert(Math.floor(number / base)) + (number % base < 10 ? number % base : String.fromCharCode(87 + number % base));
+  return function (number) {
+    return number.toString(base);
   };
 };
