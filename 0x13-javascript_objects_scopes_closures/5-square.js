7ae2b9c53fe30034ef8c5f9f3fa817f31a8c29bf
John Gaitho <johngaitho05@gmail.com>
2023-11-14 21:41:03 +0300

    square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,7 +4,7 @@ const Rectangle = require('./4-rectangle');
 
 // Square class that inherits from Rectangle
 class Square extends Rectangle {
-  constructor(size) {
+  constructor (size) {
     super(size, size); // Call the Rectangle constructor with the size for both width and height
   }
 }
