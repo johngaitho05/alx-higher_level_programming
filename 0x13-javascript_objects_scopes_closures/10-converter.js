7071a92f4cbbb872b2d9a5ae4c15f46d04e97d7a
John Gaitho <johngaitho05@gmail.com>
2023-11-14 22:27:18 +0300

    convert to base

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
+
 exports.converter = function (base) {
   return function convert (number) {
-    if (number === 0) {
-      return '0';
-    } else {
-      const remainder = number % base;
-      const quotient = Math.floor(number / base);
-      const convertedDigit = remainder < 10 ? remainder : String.fromCharCode(87 + remainder);
-      return convert(quotient) + convertedDigit;
-    }
+    return number === 0 ? '0' : convert(Math.floor(number / base)) + (number % base < 10 ? number % base : String.fromCharCode(87 + number % base));
   };
 };
