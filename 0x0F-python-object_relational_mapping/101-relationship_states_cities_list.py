9c7f5239a620af16189e19579048c6b559ae8dd2
John Gaitho <johngaitho05@gmail.com>
2023-11-19 18:18:46 +0300

    refactor

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Script to list all State objects and corresponding City objects
-from the database hbtn_0e_101_usa"""
+"""Script to list all State objects and corresponding City objects from the database hbtn_0e_101_usa"""
 
 import sys
 from sqlalchemy import create_engine
@@ -9,13 +8,17 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == '__main__':
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
     # Database connection parameters
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Create a connection to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(username, password, database),
-                           pool_pre_ping=True)
+                           format(username, password, database), pool_pre_ping=True)
 
     # Create the tables
     Base.metadata.create_all(engine)
@@ -24,19 +27,16 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query to get all State objects and their corresponding City objects
-    states_cities = (session.query(State, City)
-                     .join(City, State.id == City.state_id)
-                     .order_by(State.id, City.id)
-                     .all())
+    # Query to get all State objects and their corresponding City objects using .cities
+    states = (session.query(State)
+              .order_by(State.id)
+              .all())
 
     # Print the results
-    current_state_id = None
-    for state, city in states_cities:
-        if state.id != current_state_id:
-            print("{}: {}".format(state.id, state.name))
-            current_state_id = state.id
-        print("\t{}: {}".format(city.id, city.name))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
 
     # Close the session
     session.close()
