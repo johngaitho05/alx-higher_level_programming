d4d3d27179d15cec031c86f7bcd006ee017a8def
John Gaitho <johngaitho05@gmail.com>
2023-11-19 13:47:00 +0300

    refactor

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,10 +5,12 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
+    host = 'localhost'
+    port = 3306
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(user=user, password=password, database=database)
+    db = MySQLdb.connect(host=host, port=port, user=user, password=password, database=database)
     cr = db.cursor()
     cr.execute("""SELECT id, name FROM states""")
     for rec in cr.fetchall():
