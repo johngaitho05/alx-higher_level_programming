29a4da3e7e571f86a7a5687a9d0394de2f68b7d7
John Gaitho <johngaitho05@gmail.com>
2023-11-19 13:47:54 +0300

    refactor

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     database = sys.argv[3]
     db = MySQLdb.connect(host=host, port=port, user=user, password=password, database=database)
     cr = db.cursor()
-    cr.execute("""SELECT id, name FROM states""")
+    cr.execute("""SELECT id, name FROM states ORDER BY id ASC""")
     for rec in cr.fetchall():
         print(rec)
