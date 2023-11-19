4c0500d94f8b37f65be0e622815237f35a8f79a2
John Gaitho <johngaitho05@gmail.com>
2023-11-19 15:47:24 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
 This module  lists all states from the database hbtn_0e_0_usa
-whose name matches the passed argument
-It uses parametized query to prevent SQL injection
+whose name matches the passed argument and is safe from SQL injection
 """
 import sys
+
 import MySQLdb
 
 if __name__ == '__main__':
@@ -14,27 +14,13 @@ if __name__ == '__main__':
     password = sys.argv[2]
     database = sys.argv[3]
     sname = sys.argv[4]
-    db = None
-    cr = None
-    try:
-        # Connect to the database
-        db = MySQLdb.connect(host=host, port=port, user=user,
-                             password=password, db=database, charset='utf8')
-        cr = db.cursor()
-
-        # Use parameterized query to prevent SQL injection
-        query = """SELECT id, name FROM states WHERE BINARY
-        name=%s ORDER BY id ASC"""
-        cr.execute(query, (sname,))
-        # Fetch and print results
-        for rec in cr.fetchall():
-            print(rec)
-
-    except MySQLdb.Error as e:
-        pass
-    finally:
-        # Close cursor and database connection
-        if cr:
-            cr.close()
-        if db:
-            db.close()
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         password=password, db=database, charset='utf8')
+    cr = db.cursor()
+    query = """SELECT id, name FROM states
+    WHERE name=%s ORDER BY id ASC"""
+    cr.execute(query, (sname, ))
+    for rec in cr.fetchall():
+        print(rec)
+    cr.close()
+    db.close()
