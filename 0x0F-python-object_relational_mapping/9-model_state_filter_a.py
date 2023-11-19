eb3d7bb7730d3a2a1c88b28ed075e3dbb9b9decf
John Gaitho <johngaitho05@gmail.com>
2023-11-19 17:04:49 +0300

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,10 +8,6 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == '__main__':
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
 
     # Database connection parameters
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
