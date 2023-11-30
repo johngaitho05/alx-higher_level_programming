2d6948eb6d1c2adfbc802cb13bb580561b7b72d3
John Gaitho <johngaitho05@gmail.com>
2023-11-30 20:37:09 +0300

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
 url="$1"
 email="test@gmail.com"
 subject="I will always be here for PLD"
