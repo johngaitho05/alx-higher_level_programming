c6b15e3a84252c6e4e78942d9bf44f445ec0b6e2
John Gaitho <johngaitho05@gmail.com>
2023-12-01 23:25:23 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,30 +5,16 @@ letter as a parameter."""
 import requests
 import sys
 
-# Check if a letter is provided as a command-line argument
-if len(sys.argv) == 2:
-    letter = sys.argv[1]
-else:
-    letter = ""
-
-# Prepare the data to be sent in the POST request
-data = {'q': letter}
-
-try:
-    # Send a POST request with the letter parameter
-    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
-
-    # Check if the response body is properly JSON formatted and not empty
+if __name__ == "__main__":
     try:
-        result_json = response.json()
-        if result_json:
-            print(f"[{result_json.get('id')}] {result_json.get('name')}")
-        else:
+        payload = {'q': sys.argv[1]}
+        r = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+        res = r.json()
+        if len(res) == 0:
             print("No result")
+        else:
+            print("[{}] {}".format(res['id'], res['name']))
     except ValueError:
         print("Not a valid JSON")
-
-except requests.RequestException as e:
-    print(f"Error sending the POST request: {e}")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
+    except IndexError:
+        print("No result")
